1a9900edc4bc535baf7effa091c119e9b7abb564
kaleb <82520535+kaleb-hub@users.noreply.github.com>
2021-04-15 11:49:56 +0300

    Update home.php

--- home.php
+++ home.php
@@ -8,7 +8,7 @@
 <img src="develop.png" style
 ="width=100px; height=50px;">
 <h3> this is first page for employee and administrator</h3>
-	<form action="login.php">
+	<form action="login.php" method="post" enctype="multipart/form-data">
 		<input type="text" name="name" placeholder="your name"/>
 		<input type="password" name="password">
 		<input type="submit" name="submit" value="LOGIN">

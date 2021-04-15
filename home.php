d8423946ccdf3d3ea54607c2b6043f769afcf042
kaleb <82520535+kaleb-hub@users.noreply.github.com>
2021-04-15 11:49:02 +0300

    Update home.php

--- home.php
+++ home.php
@@ -7,6 +7,11 @@
 <p>THIS IS HOME PAGE </p>
 <img src="develop.png" style
 ="width=100px; height=50px;">
-<h3> is first page for employee and administrator</h3>
+<h3> this is first page for employee and administrator</h3>
+	<form action="login.php">
+		<input type="text" name="name" placeholder="your name"/>
+		<input type="password" name="password">
+		<input type="submit" name="submit" value="LOGIN">
+	</form>
 </body>
 </html>
